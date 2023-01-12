2bbab777ba864fb5f73bab911187c3f01b69fb11
Jakub Marchewka <jakub.marchewka@septemonline.com>
2023-01-12 17:31:31 +0100

    bill

--- src/Enum/BillTypeEnum.php
+++ src/Enum/BillTypeEnum.php
@@ -7,12 +7,20 @@ namespace App\Enum;
 enum BillTypeEnum: string
 {
     case OTHER = 'other';
+    case ELECTRICITY = 'electricity';
+    case ASSOCIATION = 'association';
+    case GAS = 'gas';
+    case INTERNET = 'internet';
 
     public function getLabel(): string
     {
         return match($this)
         {
+            self::ELECTRICITY => 'Rachunek za prąd',
             self::OTHER => 'Inne',
+            self::ASSOCIATION => 'Wspólnota',
+            self::GAS => 'Rachunek za gaz',
+            self::INTERNET => 'Rachunek za internet',
         };
     }
 }
